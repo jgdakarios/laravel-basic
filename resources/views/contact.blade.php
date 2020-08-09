e087f61ecd9631736466c6bd923beff1eab2f59d
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-09 19:21:40 -0400

    Envio de formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,16 @@
 @extends('layout')
 
+@section('title', 'Hello! | Contact')
+
 @section('content')
+    <h1>Contact</h1>
 
-<h1>Contact</h1>
-
+    <form method="POST" action="{{ route('contact') }}">
+        @csrf
+        <input name="name" placeholder="Nombre..."><br>
+        <input type="email" name="email" placeholder="Email..."><br>
+        <input name="subject" placeholder="Asunto..."><br>
+        <textarea name="content" placeholder="Mensaje..."></textarea><br>
+        <button>Enviar</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::view('/', 'home')->name('home');
 Route::view('about', 'about')->name('about');
 Route::get('portfolio', 'PortfolioController@index')->name('portfolio');
 Route::view('contact', 'contact')->name('contact');
+
+
+Route::post('contact', 'MessagesController@store');
