8210982e42c1ef38a65facffb9e152be55cf3f7d
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-09 14:45:45 -0400

    Estructuras de control con Blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
-<h1>Portfolio</h1>
+    <h1>Portfolio</h1>
+
+    @forelse($portfolio as $portfolioItem)
+        <li>{{ $portfolioItem['title'] }} <small>{{ $loop->first ? 'Es el primero' : '' }}</small></li>
+    @empty
+        <li>No hay proyectos para mostrar</li>
+    @endforelse
 
 @endsection
