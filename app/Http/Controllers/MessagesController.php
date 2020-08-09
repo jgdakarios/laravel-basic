036abd78a96045f92092c990b9ef6187f87c5d1e
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-09 19:43:35 -0400

    Validando formularios

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -8,6 +8,13 @@ class MessagesController extends Controller
 {
     public function store(Request $request)
     {
-        return $request;
+        request()->validate([
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'subject'   => 'required',
+            'content'   => 'required|min:3',
+        ]);
+
+        return 'Datos validados';
     }
 }
