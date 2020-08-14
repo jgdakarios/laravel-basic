682bf65daad44744ad66d50ce25475f16ef2bcdf
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-14 11:13:16 -0400

    Eloquent: eliminar registros

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,7 +5,13 @@
 @section('content')
 
     <h1>{{ $project->title }}</h1>
+
     <a href="{{ route('projects.edit', $project) }}">Editar proyecto</a>
+
+    <form method="POST" action="{{ route('projects.destroy', $project) }}">
+        @csrf @method('DELETE')
+        <button>Eliminar</button>
+    </form>
 
     <p>{{ $project->description }}</p>
     <p>{{ $project->created_at->diffForHumans() }}</p>
--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+# App Languaje
 App::setLocale('es');
 
+# Home
 Route::view('/', 'home')->name('home');
+
+# About
 Route::view('about', 'about')->name('about');
 
-# Projects routes
+# Projectss
 Route::get('project', 'ProjectController@index')->name('projects.index');
 Route::get('project/create', 'ProjectController@create')->name('projects.create');
 Route::get('project/{project}', 'ProjectController@show')->name('projects.show');
 Route::get('project/{project}/edit', 'ProjectController@edit')->name('projects.edit');
-
 Route::patch('project/{project}', 'ProjectController@update')->name('projects.update');
 Route::post('project', 'ProjectController@store')->name('projects.store');
+Route::delete('project/{project}', 'ProjectController@destroy')->name('projects.destroy');
 
+# Contact
 Route::view('contact', 'contact')->name('contact');
 Route::post('contact', 'MessageController@store')->name('message.store');
