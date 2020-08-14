7204f58ba452cf62fb6c5036b9c315afaeb1c951
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-14 12:02:33 -0400

    Route Resource

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,16 @@ Route::view('/', 'home')->name('home');
 # About
 Route::view('about', 'about')->name('about');
 
-# Projectss
-Route::get('project', 'ProjectController@index')->name('projects.index');
-Route::get('project/create', 'ProjectController@create')->name('projects.create');
-Route::get('project/{project}', 'ProjectController@show')->name('projects.show');
-Route::get('project/{project}/edit', 'ProjectController@edit')->name('projects.edit');
-Route::patch('project/{project}', 'ProjectController@update')->name('projects.update');
-Route::post('project', 'ProjectController@store')->name('projects.store');
-Route::delete('project/{project}', 'ProjectController@destroy')->name('projects.destroy');
+# Projects
+Route::resource('projects', 'ProjectController');
+
+// Route::get('project', 'ProjectController@index')->name('projects.index');
+// Route::get('project/create', 'ProjectController@create')->name('projects.create');
+// Route::get('project/{project}', 'ProjectController@show')->name('projects.show');
+// Route::get('project/{project}/edit', 'ProjectController@edit')->name('projects.edit');
+// Route::patch('project/{project}', 'ProjectController@update')->name('projects.update');
+// Route::post('project', 'ProjectController@store')->name('projects.store');
+// Route::delete('project/{project}', 'ProjectController@destroy')->name('projects.destroy');
 
 # Contact
 Route::view('contact', 'contact')->name('contact');
