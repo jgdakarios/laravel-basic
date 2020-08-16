e788fb3ca763f493bf18f7067dce33b5cfa89ecf
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-16 10:01:17 -0400

    Agregando estilos iniciales de bootstrap al layout, content y footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,18 +1,40 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>@yield('title', 'Hello!')</title>
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
         <style>
-            .active a{
-                color: red;
-                text-decoration: none;
+            .h-screen {
+                height: 100vh;
             }
         </style>
     </head>
     <body>
-        @include('partials.nav')
-        @include('partials.session-status')
-        @yield('content')
+        <div id="app" class="d-flex flex-column h-screen justify-content-between">
+            <header>
+                @include('partials.nav')
+                @include('partials.session-status')
+            </header>
+
+            <main>
+                @yield('content')
+            </main>
+
+            <footer class="bg-white text-center text-black-50 py-3 shadow">
+                {{ config('app.name') }} | Copyright @ {{ date('Y') }}
+            </footer>
+        </div>
+
+        
+        
+        
     </body>
 </html>
